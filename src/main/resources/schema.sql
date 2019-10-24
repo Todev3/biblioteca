dede495b91287ffd09ad87f306ca1df54f103b81
Emerson Abdias <emerson.abdias@cerc.inf.br>
2019-10-24 17:20:43 -0300

    Fix sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,13 @@
-CREATE SEQUENCE LIVRO_SEQUENCE
-    START WITH 1
-    INCREMENT BY 1;
+NOT EXISTS(SELECT 1 FROM livro) THEN create table livro (id number(10) not null, autor varchar2(50), nome varchar2(50), ano date, constraint livro_pk primary key (id));
+
 
 create table livro (
   id number(10) not null,
   autor varchar2(50),
   nome varchar2(50),
-  ano date,
+  ano TIMESTAMP,
   constraint livro_pk primary key (id)
 );
+
+
+CREATE SEQUENCE LIVRO_SEQUENCE START WITH 1 INCREMENT BY 1
